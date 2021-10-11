cfbfa29a97758d92dd5c1f9d8b51050ab8368b9f
Keith Rutkowski <keith@analytech-solutions.com>
2021-10-11 10:01:58 -0400

    Fix CSFML to use Julia binary wrappers for GL/GLU

--- test/CSFML.jl
+++ test/CSFML.jl
@@ -1,8 +1,16 @@
 module libcsfml
 	using CBinding
+	using Libglvnd_jll
+	using GLU_jll
 	using CSFML_jll
 	
 	c`$([
+		"-I$(Libglvnd_jll.artifact_dir)/include",
+		"-L$(dirname(Libglvnd_jll.libGL_path))", "-lGL",
+		
+		"-I$(GLU_jll.artifact_dir)/include",
+		"-L$(dirname(GLU_jll.libGLU_path))", "-lGLU",
+		
 		"-I$(CSFML_jll.artifact_dir)/include",
 		"-L$(dirname(CSFML_jll.libcsfml_system_path))", "-lcsfml-system",
 		"-L$(dirname(CSFML_jll.libcsfml_audio_path))", "-lcsfml-audio",
